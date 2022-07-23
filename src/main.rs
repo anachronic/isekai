739929628fcc696d38b63e7c8f54e4e40ee58d39
Nicolás Salas <nsalas@fastmail.com>
2022-07-22 22:58:29 -0400

    feat: redirect everything to wiki

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::Redirect;
+
+#[get("/<redir>")]
+async fn redirect(redir: String) -> Redirect {
+    println!("called with {}", redir);
+
+    Redirect::permanent("https://anachronic.io/wiki")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![redirect])
 }
