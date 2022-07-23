8e57a5256a9bd11799ad52a07ff9569d9825ba17
Nicolás Salas <nsalas@fastmail.com>
2022-07-22 23:34:12 -0400

    feat: look up redirects in redis

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::response::Redirect;
+use rocket::{response::Redirect, http::Status};
+use redis::Commands;
 
 #[get("/<redir>")]
-async fn redirect(redir: String) -> Redirect {
-    println!("called with {}", redir);
+async fn redirect(redir: String) -> Result<Redirect, Status> {
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_connection().unwrap();
 
-    Redirect::permanent("https://anachronic.io/wiki")
+    let something: Result<String, _> = con.get(&redir);
+
+    match something {
+        Err(_) => {
+            println!("Redis errored");
+            return Err(Status::NotFound)
+        },
+        Ok(url) => {
+            return Ok(Redirect::permanent(url))
+        }
+    }
 }
 
 #[launch]
